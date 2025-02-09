b4501d1802338e0094152bbd2c2cd9ffe7e36e33
Kristo Aun <kristoaun@live.com>
2025-02-09 13:23:01 +0200

    user=root

--- filesystem/mnt/supervisor.sh
+++ filesystem/mnt/supervisor.sh
@@ -4,6 +4,7 @@ echo "Configure supervisor"
 cat <<EOF > /etc/supervisor/conf.d/supervisord.conf
 [supervisord]
 nodaemon=true
+user=root
 
 [program:openvpn]
 command=openvpn --config /etc/openvpn/server.conf
